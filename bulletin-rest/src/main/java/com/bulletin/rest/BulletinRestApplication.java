b68e33963bd08df14832bc5ef4570c68d828d997
savannah030 <savannah030@naver.com>
2021-12-26 07:20:08 +0900

    feat: 게시판 기본기능(CRUD) 구현

--- bulletin-rest/src/main/java/com/bulletin/rest/BulletinRestApplication.java
+++ bulletin-rest/src/main/java/com/bulletin/rest/BulletinRestApplication.java
@@ -1,20 +1,22 @@
-package com.bulletin;
+package com.bulletin.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ComponentScan;
 
-@RestController
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.bulletin.rest")
 public class BulletinRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BulletinRestApplication.class, args);
 	}
 
+
+	/*
 	@GetMapping
 	public String HelloWorld(){
-		return "first feature branch";
+		return "Hello Rest";
 	}
+	*/
 }
