8b45a5fa032c105725b3ce9df7e4044f67cc2b3c
savannah030 <savannah030@naver.com>
2022-01-03 19:51:28 +0900

    fix: 오타 수정

--- bulletin-web/src/main/java/com/web/domain/User.java
+++ bulletin-web/src/main/java/com/web/domain/User.java
@@ -45,11 +45,12 @@ public class User implements Serializable {
     private LocalDateTime updatedDate;
 
     @Builder
-    public User(String name, String password, String email, String pincipal, SocialType socialType, LocalDateTime createdDate, LocalDateTime updatedDate) {
+    public User(Long idx, String name, String password, String email, String principal, SocialType socialType, LocalDateTime createdDate, LocalDateTime updatedDate) {
+        this.idx = idx;
         this.name = name;
         this.password = password;
         this.email = email;
-        this.principal = pincipal;
+        this.principal = principal;
         this.socialType = socialType;
         this.createdDate = createdDate;
         this.updatedDate = updatedDate;
